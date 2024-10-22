c8f30ae166ee1f4d1f048d7583a04e550e906eba
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 14:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const inputForm= document.querySelector("#input-form");
+
+inputForm.addEventListener("submit", e => {
+	e.preventDefault();
+	const id= document.querySelector("#block_id").value;
+	const color= document.querySelector("#colour_id").value;
+	const block= document.querySelector(`#\\3${id}`);
+	block.style.backgroundColor = color;
+})
+
+inputForm.addEventListener("reset", e => {
+	const blocks= document.querySelectorAll(".grid-item");
+	blocks.forEach(block => {
+		block.style.backgroundColor = "transparent";
+	})
+})
+
+
+
+
+
+
+
